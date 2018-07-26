5b429b94cc09268468664062691560be3b3399b8
cloudy-liu <cloudy.liuu@gmail.com>
2018-07-27 01:04:40 +0800

    solved  leetcode-807 V2

--- OJ/leetcode/807-Max Increase to Keep City Skyline/MaxIncreaseToKeepCitySkyline.java
+++ OJ/leetcode/807-Max Increase to Keep City Skyline/MaxIncreaseToKeepCitySkyline.java
@@ -55,66 +55,70 @@ public class MaxIncreaseToKeepCitySkyline {
          * V1: 暴力法 遍历行列时，求取该行该列的最大值，然后取两者的较小值，和该矩阵中的值做差
          * 同时，列的取值，通过可缓存下来，该解法 Java 13ms, 击败 8.74% 。垫底的速度。囧。
          */
-        int rowLen = grid.length;
-        int colLen = grid[0].length;
-        int rowMax = 0;
-        int colMax = 0;
-        int rst = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < rowLen; i++) {
-            // rowMax
-            rowMax = 0;
-            for (int k = 0; k < colLen; k++) {
-                if (grid[i][k] > rowMax) {
-                    rowMax = grid[i][k];
-                }
-            }
-            for (int j = 0; j < colLen; j++) {
-                // colMax
-                if (!map.containsKey(j)) {
-                    colMax = 0;
-                    for (int m = 0; m < rowLen; m++) {
-                        if (grid[m][j] > colMax) {
-                            colMax = grid[m][j];
-                        }
-                    }
-                    map.put(j, colMax);
-                } else {
-                    colMax = map.get(j);
-                }
+        // int rowLen = grid.length;
+        // int colLen = grid[0].length;
+        // int rowMax = 0;
+        // int colMax = 0;
+        // int rst = 0;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for (int i = 0; i < rowLen; i++) {
+        // // rowMax
+        // rowMax = 0;
+        // for (int k = 0; k < colLen; k++) {
+        // if (grid[i][k] > rowMax) {
+        // rowMax = grid[i][k];
+        // }
+        // }
+        // for (int j = 0; j < colLen; j++) {
+        // // colMax
+        // if (!map.containsKey(j)) {
+        // colMax = 0;
+        // for (int m = 0; m < rowLen; m++) {
+        // if (grid[m][j] > colMax) {
+        // colMax = grid[m][j];
+        // }
+        // }
+        // map.put(j, colMax);
+        // } else {
+        // colMax = map.get(j);
+        // }
 
-                int DiffValue = Math.min(rowMax, colMax);
-                rst += (DiffValue - grid[i][j]);
-            }
-        }
-        return rst;
+        // int DiffValue = Math.min(rowMax, colMax);
+        // rst += (DiffValue - grid[i][j]);
+        // }
+        // }
+        // return rst;
 
         /**
-         * V2: V1每列 max 值重复计算了
+         * V2: 先一次性计算出，每行，每列的最大值，这样做两次 O(m*n)复杂度
          * 
          */
 
-        // int rowLen = grid.length;
-        // int colLen = grid[0].length;
-        // int[] rowMax = new int[rowLen];
-        // int[] colMax = new int[colLen];
-
-        // for (int i = 0; i < rowLen; i++) {
-        //     for (int j = 0; j < colLen; j++) {
-        //         rowMax[i] = Math.max(rowMax[i], grid[i][j]);
-        //         colMax[i] = Math.max(colMax[i], grid[i][j + rowLen]);
-        //     }
-        // }
+        int rowLen = grid.length;
+        int colLen = grid[0].length;
+        int[] rowMax = new int[rowLen];
+        int[] colMax = new int[colLen];
+        int rst = 0;
+        for (int i = 0; i < rowLen; i++) {
+            for (int j = 0; j < colLen; j++) {
+                rowMax[i] = Math.max(rowMax[i], grid[i][j]);//存储每行的最大值 i为行号
+                colMax[j] = Math.max(colMax[j], grid[i][j]);//存储每列的最大值，j为列号
+            }
+        }
+        for (int i = 0; i < rowLen; i++) {
+            for (int j = 0; j < colLen; j++) {
+                rst += Math.min(rowMax[i], colMax[j]) - grid[i][j];
+            }
+        }
+        return rst;
     }
 
     public static void main(String[] args) {
         MaxIncreaseToKeepCitySkyline solution = new MaxIncreaseToKeepCitySkyline();
-        int[][] data = new int[][] { new int[] { 3, 0, 8, 4 }, new int[] { 2, 4, 5, 7 }, new int[] { 9, 2, 6, 3 },
-                new int[] { 0, 3, 1, 0 } };
-        // int[][] data = new int[][] {
-        // new int[]{0, 0},
-        // new int[]{0,0}
-        // };
+        // int[][] data = new int[][] { new int[] { 3, 0, 8, 4 }, new int[] { 2, 4, 5, 7
+        // }, new int[] { 9, 2, 6, 3 },
+        // new int[] { 0, 3, 1, 0 } };
+        int[][] data = new int[][] { new int[] { 59, 88, 44 }, new int[] { 3, 18, 38 }, new int[] { 21, 26, 51 } };
         System.out.println(solution.maxIncreaseKeepingSkyline(data));
 
     }
