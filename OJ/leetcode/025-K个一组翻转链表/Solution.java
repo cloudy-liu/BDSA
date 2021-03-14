2e4dffad7ac302ad942ddee75cdbf6a279dddb39
cloudy-liu <cloudy.liuu@gmail.com>
2021-03-14 21:13:43 +0800

    update leetcode-025 reverseKGroup

--- OJ/leetcode/025-K个一组翻转链表/Solution.java
+++ OJ/leetcode/025-K个一组翻转链表/Solution.java
@@ -2,19 +2,15 @@
 /**
  * https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
  * 
- * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
-k 是一个正整数，它的值小于或等于链表的长度。
-如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序
-
-输入：head = [1,2,3,4,5], k = 2
-输出：[2,1,4,3,5]
-
-输入：head = [1,2,3,4,5], k = 3
-输出：[3,2,1,4,5]
-
-输入：head = [1], k = 1
-输出：[1]
-
+ * 给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。 k 是一个正整数，它的值小于或等于链表的长度。
+ * 如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序
+ * 
+ * 输入：head = [1,2,3,4,5], k = 2 输出：[2,1,4,3,5]
+ * 
+ * 输入：head = [1,2,3,4,5], k = 3 输出：[3,2,1,4,5]
+ * 
+ * 输入：head = [1], k = 1 输出：[1]
+ * 
  * Date: 2021/3/10
  */
 
@@ -29,8 +25,8 @@ class ListNode {
     public static void travalList(ListNode head) {
         while (head != null) {
             System.out.print(head.val + "-->");
+            head = head.next;
         }
-        head = head.next;
         System.out.println("");
     }
 
@@ -55,17 +51,18 @@ class ListNode {
 
 public class Solution {
 
-    public void reverseListByHeadTail(ListNode pre, ListNode head, ListNode tail) {
-        ListNode newHead = null;
-
-        ListNode pre = tail.next;
+    public ListNode reverseListByNode(ListNode start, ListNode end) {
+        // [a,b)
+        ListNode cur = start;
+        ListNode pre = null;
         ListNode next = null;
-        while (head != null) {
-            next = head.next;// save
-            head.next = pre;
-            pre = head;
-            head = next;
+        while (cur != end) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
         }
+        return pre;
     }
 
     public ListNode reverseKGroup(ListNode head, int k) {
@@ -73,31 +70,31 @@ public class Solution {
             return head;
         }
 
-        ListNode reverseHead = null;
-        ListNode newHead = null;
-        int i = 1;
-        ListNode cur = head;
-        while (cur != null) {
-            if (reverseHead == null) {
-                reverseHead = cur;
+        ListNode left = head;
+        ListNode right = head;
+        for (int i = 0; i < k; i++) {
+            if (right == null) {
+                return left;
             }
-            i++;
-            cur = cur.next;
-            if (i == k) {
-                ListNdoe rst = reverseListByHeadTail(reverseHead, cur);
-                if (newHead == null) {
-                    newHead = rst;
-                }
-                i = 1;
-                reverseHead = null;
-            }
-
+            right = right.next;
         }
+
+        ListNode newHead = reverseListByNode(left, right);
+        left.next = reverseKGroup(right, k);
         return newHead;
     }
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Solution s = new Solution();
+        ListNode head = ListNode.buildList(new int[] { 1, 2, 3, 4, 5 });
+        ListNode.travalList(s.reverseKGroup(head, 2)); // [2,1,4,3,5]
+
+        head = ListNode.buildList(new int[] { 1, 2, 3, 4, 5 });
+        ListNode.travalList(s.reverseKGroup(head, 3)); // [3,2,1,4,5]
+
+        head = ListNode.buildList(new int[] { 1 });
+        ListNode.travalList(s.reverseKGroup(head, 1)); // [1]
+
     }
 
 }
